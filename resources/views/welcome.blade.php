7263aac5203fa94831ce4d488ba6b147622362ea
mar1 <marwantoufini1234@gmail.com>
2023-10-15 14:41:26 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,4 @@
 @section('title', 'home')
 @section('content')
     <p class="text-4xl text-center uppercase mt-5">Forum app</p>
-    <ul>
-        <li>marwan</li>
-        <li>akram</li>
-    </ul>
 @endsection
