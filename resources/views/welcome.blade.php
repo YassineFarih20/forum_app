815e2b6ac27547ab0d954eb42d1f0e7e8294ec23
AKRAM <63755025+Skyreex@users.noreply.github.com>
2023-10-15 23:30:37 +0100

    fixed DB

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,4 @@
 @section('title', 'home')
 @section('content')
     <p class="text-4xl text-center uppercase mt-5">Forum app</p>
-     <p class="text-4xl text-center uppercase mt-5">modif</p>
-    <h1>test marwan</h1>
-    <ul>
-        <li>marwan</li>
-        <li>akram</li>
-    </ul>
 @endsection
