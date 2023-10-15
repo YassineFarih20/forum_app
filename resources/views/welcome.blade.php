080ec463f951424e8a66108c8b9c3a97bf19f168
Yassine Farih <yassinefarih27@gmail.com>
2023-10-15 15:06:50 +0100

    modif test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,4 +3,5 @@
 @section('title', 'home')
 @section('content')
     <p class="text-4xl text-center uppercase mt-5">Forum app</p>
+    <p class="text-4xl text-center uppercase mt-5">modif</p>
 @endsection
