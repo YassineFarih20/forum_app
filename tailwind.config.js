b1a4e9e104de99fb0293bec0d323ae430eee0a5d
AKRAM <63755025+Skyreex@users.noreply.github.com>
2023-10-15 11:44:10 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['resources/views/**/*.blade.php', 'resources/js/**/*.js'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
